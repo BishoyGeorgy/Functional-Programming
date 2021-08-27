fb70872ecdbcd002753cfe3bf39639ec9527c0e3
BishoyGeorgy <bishoy.georgy93@gmail.com>
2021-08-27 16:17:04 +0200

    Sequence Operations using stream functions

---
+++ FPExamples/src/main/java/com/fp/session2/SequencedOperations.java
@@ -0,0 +1,69 @@
+package com.fp.session2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Bishoy Georgy
+ * @version 1.0
+ * @date 27/08/2021
+ */
+public class SequencedOperations {
+
+    public static void main (String[] args) {
+        List<Double> numbers = new ArrayList<>();
+        numbers.add(7.0);
+        numbers.add(4.0);
+        numbers.add(5.0);
+        numbers.add(6.0);
+        numbers.add(3.0);
+        numbers.add(8.0);
+        numbers.add(10.0);
+
+        //1st Example
+        List<Double> result1 = numbers.stream().map(SequencedOperations::addOne)
+                                               .map(SequencedOperations::square)
+                                               .map(SequencedOperations::subtractTen)
+                                               .collect(Collectors.toList());
+
+        result1.forEach(number -> System.out.println(number));
+        System.out.println("------------------");
+
+        //2nd Example
+        List<Double> result2 = numbers.stream().map(SequencedOperations::addOne)
+                                               .map(SequencedOperations::square)
+                                               .filter( x -> x < 20)
+                                               .map(SequencedOperations::subtractTen)
+                                               .collect(Collectors.toList());
+
+        result2.forEach(number -> System.out.println(number));
+        System.out.println("------------------");
+
+        //3rd Example
+        List<Double> result3 = numbers.stream().map(SequencedOperations::addOne)
+                                               .map(SequencedOperations::square)
+                                               .filter( x -> x < 70)
+                                               .sorted()
+                                               .limit(2)
+                                               .map(SequencedOperations::subtractTen)
+                                               .collect(Collectors.toList());
+
+        result3.forEach(number -> System.out.println(number));
+        System.out.println("------------------");
+
+
+    }
+
+    private static double subtractTen(double x) {
+        return x - 10.0;
+    }
+
+    private static double square(double x) {
+        return Math.pow(x, 2);
+    }
+
+    private static double addOne(double x) {
+        return x + 1.0;
+    }
+}
